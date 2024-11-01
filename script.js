3f8b93ef31b875453dc1f97c162f00262ab3bfea
Vro2000 <veronique.urbino@hotmail.fr>
2024-11-01 19:54:08 +0100

    version de présentation

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
 // gestion des parallaxes _________________________________
 window.addEventListener('scroll', function () {
   const scrollY = window.scrollY;
-
   const medium = document.querySelector('.medium');
   const fast = document.querySelector('.fast');
-  // const contenu = document.querySelector('.contenu');
-  // Modifier la vitesse
+  // pour modifier la vitesse
   medium.style.transform = `translateY(${scrollY * 0.8}px)`; // Moyen
   fast.style.transform = `translateY(${scrollY * 0.4}px)`; // Rapide
-  // contenu.style.transform = `translateY(${scrollY * 0.7}px)`; // Contenu (technologies + projets)
 });
 
 // color picker ___________________________________________
@@ -19,67 +16,68 @@ colorPicker.addEventListener('input', function () {
   fond.style.backgroundColor = this.value;
 });
 
+document.querySelector('.color-picker input').value = "#800080" ;
+
 // Modale 'voir plus' _____________________________________________
 
-// Ouvre la modale quand on clique sur "Voir plus"
+const modal = document.getElementById('modal');
+
 document.getElementById('voir-plus').addEventListener('click', function (event) {
-  event.stopPropagation(); // Empêche la propagation pour éviter de déclencher la fermeture
-  const modal = document.getElementById('modal');
+  event.stopPropagation(); 
   modal.style.display = 'flex'; // Affiche la modale
 });
 
-// Ferme la modale quand on clique sur la croix
 document.querySelector('.close').addEventListener('click', function () {
-  const modal = document.getElementById('modal');
-  modal.style.display = 'none';
+  modal.style.display = 'none'; // Ferme la modale quand on clique sur la croix
 });
 
-// Empêche les clics à l'intérieur de la modale de fermer la modale
-document.getElementById('modal').addEventListener('click', function (event) {
-  event.stopPropagation();
+modal.addEventListener('click', function (event) {
+  event.stopPropagation(); // Empêche les clics à l'intérieur de la modale de fermer la modale
 });
 
-// Ferme la modale si on clique en dehors de la modale
 window.addEventListener('click', function () {
-  const modal = document.getElementById('modal');
-  modal.style.display = 'none';
+  modal.style.display = 'none'; // Ferme la modale si on clique en dehors de la modale
 });
 
 // Ferme la modale si on scroll au-delà d'une certaine hauteur
 window.addEventListener('scroll', function () {
-  const modal = document.getElementById('modal');
-  const scrollPosition = window.scrollY || document.documentElement.scrollTop;
+  const scrollPosition = window.scrollY;
 
-  if (scrollPosition > 700) {
+  if (modal.style.display === 'flex' && scrollPosition > 700) {
     modal.style.display = 'none';
   }
 });
 
-
 // collapse containers ___________________________________
 
-// Fonction pour basculer entre les onglets
 function openOnglet(evt, ongletName) {
-  // Masquer tous les contenus d'onglets
+
+  const ongletButtons = document.querySelectorAll('.onglet-button');
+  ongletButtons.forEach((button) => {
+    button.classList.remove('active'); // Retire la classe 'active' de tous les liens d'onglets
+  });
+
   const ongletContents = document.querySelectorAll('.onglet-content');
   ongletContents.forEach((content) => {
-    content.style.display = 'none';
+    content.style.display = 'none';   // Masque tous les contenus d'onglets
   });
-
-  // Retirer la classe 'active' de tous les liens d'onglets
-  const ongletLinks = document.querySelectorAll('.onglet-link');
-  ongletLinks.forEach((link) => {
-    link.classList.remove('active');
-  });
-
-  // Afficher le contenu de l'onglet sélectionné et ajouter 'active' au bouton
-  document.getElementById(ongletName).style.display = 'flex';
-  evt.currentTarget.classList.add('active');
+ 
+   evt.currentTarget.classList.add('active');  // Ajoute la classe 'active' au bouton cliqué
+   document.getElementById(ongletName).style.display = 'flex';// Affiche le contenu de l'onglet sélectionné
 }
 
-// Initialisation pour afficher le premier onglet
+  //  récupérer les data attributs de l'onglet cliqué
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelector('.onglet-link.active').click();
+  const ongletButtons = document.querySelectorAll('.onglet-button');
+
+  ongletButtons.forEach((button) => {
+    button.addEventListener('click', function (event) {
+      const ongletName = button.getAttribute('data-onglet');
+      openOnglet(event, ongletName);
+    });
+  });
+  
+  document.querySelector('.onglet-button.active').click(); // click automatique pour le premier onglet
 });
 
 
@@ -89,10 +87,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
 fetch('projets.json')
   .then((response) => response.json())
-  .then((projets) => {
+  .then((projetsTab) => {
     const projetsContainer = document.getElementById('projets-container');
+    const totalProjets = projetsTab.length;
     let currentIndex = 0;
-    const totalProjets = projets.length;
+
 
     // Mise à jour du compteur du carousel
     const updateCounter = () => {
@@ -102,7 +101,7 @@ fetch('projets.json')
     };
 
     // Affichage des projets
-    projets.forEach((projet) => {
+    projetsTab.forEach((projet) => {
       const projetDiv = document.createElement('div');
       projetDiv.classList.add('projet-item', 'encadre');
 
@@ -118,11 +117,7 @@ fetch('projets.json')
       // Vérifie si le lien de l'image existe
       let imageHTML = '';
       if (projet['img-site'] !== null) {
-        imageHTML = `
-          <div class="image-container">
-            <img src="${projet['img-site']}" alt="${projet.alt}">
-            <div class="image-overlay"></div>
-          </div>`;
+        imageHTML = `<img src="${projet['img-site']}" alt="${projet.alt}">`;
       } else {
         imageHTML = `<p>(Image non disponible)</p>`;
       }
@@ -150,15 +145,14 @@ fetch('projets.json')
       projetsContainer.appendChild(projetDiv);
     });
 
-    // Initialisation du compteur
-    updateCounter();
+    updateCounter();     // Initialisation du compteur
 
     // Gestion des boutons de navigation
     const nextButtons = document.querySelectorAll('.nextButton');
     const prevButtons = document.querySelectorAll('.prevButton');
 
     // Fonction pour afficher le projet en fonction de l'index
-    const EditProjet = (index) => {
+    const afficheProjet = (index) => {
       projetsContainer.scrollTop = projetsContainer.offsetHeight * index;
       updateCounter();
     };
@@ -167,7 +161,7 @@ fetch('projets.json')
     nextButtons.forEach((button) => {
       button.addEventListener('click', () => {
         currentIndex = (currentIndex + 1) % totalProjets; // Boucle au début après le dernier projet
-        EditProjet(currentIndex);
+        afficheProjet(currentIndex);
       });
     });
 
@@ -175,7 +169,7 @@ fetch('projets.json')
     prevButtons.forEach((button) => {
       button.addEventListener('click', () => {
         currentIndex = (currentIndex - 1 + totalProjets) % totalProjets; // Boucle à la fin après le premier projet
-        EditProjet(currentIndex);
+        afficheProjet(currentIndex);
       });
     });
   });
